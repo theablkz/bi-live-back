bef5b5929a908b8e2783e99568da42e8c30b8a11
abaltabekov <abaltabekov@beeline.kz>
2021-04-24 21:05:43 +0600

    api

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
