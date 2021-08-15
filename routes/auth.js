26a4deef9dbfcdbed4426ded77081d0910db8fd6
ABaltabekov <abaltabekov@beeline.kz>
2021-08-16 03:40:09 +0600

    auth fix

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-    if (req.body.user = 'YWRtaW4=S2VnJDdSc1l1V1Q4') {
+    if (req.body.user === 'YWRtaW4=S2VnJDdSc1l1V1Q4') {
         res.send('auth success');
     }else {
         res.status(403)
